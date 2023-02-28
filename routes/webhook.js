89c9cfc476760db1c9684bd9855cb1043368a38a
Muneeb T <muneeb.t@neutrinos.co>
2023-02-28 11:28:36 +0530

    Response changed

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(req.query?.hub?.challenge)
+  res.send(
+    req.query?.hub?.challenge ? req.query.hub.challenge : "Something went wrong"
+  );
 });
 
 export default router;
