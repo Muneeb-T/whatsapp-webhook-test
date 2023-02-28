f9722a8ef52d8bc46a87ac43c48242b2c1cf2f84
Muneeb T <muneeb.t@neutrinos.co>
2023-02-28 11:27:32 +0530

    Response changed

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(req.query.hub.challenge)
+  res.send(req.query?.hub?.challenge)
 });
 
 export default router;
