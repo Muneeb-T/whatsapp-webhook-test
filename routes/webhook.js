b2270a8a21bb6faa023ce590a9678f1c263f110d
Muneeb T <muneeb.t@neutrinos.co>
2023-03-01 12:59:51 +0530

    message sendig functionality added

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,14 +1,24 @@
-import express from "express";
+import express from 'express';
+import axios from 'axios';
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const response = req.query["hub.challenge"] || "Invalid challenge value";
+router.get('/', (req, res) => {
+  const response = req.query['hub.challenge'] || 'Invalid challenge value';
   res.send(response);
 });
 
-router.post('/', (req,res)=>{
-  console.log(req.body)
-  res.send(req.body)
-})
+router.post('/', (req, res) => {
+  const { from, text } = req.body?.entry[0]?.changes[0]?.messages[0];
+  const { body } = text;
+  axios
+    .post('https://tan-powerful-hummingbird.cyclic.app/dialogflow', {
+      languageCode: 'en',
+      queryText: body,
+      sessionId: from,
+    })
+    .then((data) => {
+      console.log(data)
+    });
+});
 
 export default router;
