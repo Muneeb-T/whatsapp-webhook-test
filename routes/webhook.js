750a37abe099480b72a39af4fa595d02f7baf6ae
Muneeb T <muneeb.t@neutrinos.co>
2023-03-01 14:34:32 +0530

    simple update

--- routes/webhook.js
+++ routes/webhook.js
@@ -7,18 +7,42 @@ router.get('/', (req, res) => {
   res.send(response);
 });
 
-router.post('/', (req, res) => {
+const callDialogFlow = async (queryText, sessionId) => {
+  const { data } = await axios.post(
+    'https://tan-powerful-hummingbird.cyclic.app/dialogflow',
+    {
+      languageCode: 'en',
+      queryText,
+      sessionId,
+    },
+  );
+  return data;
+};
+
+const sendMessage = async (to, message) => {
+  const phoneNumberId = '117158507969853';
+  const version = 'v16.0';
+  const { data } = await axios.post(
+    `https://graph.facebook.com/${version}/${phoneNumberId}/messages`,
+    {
+      messaging_product: 'whatsapp',
+      to,
+      text: {
+        body: message,
+      },
+    },
+  );
+  return data;
+};
+
+router.post('/', async (req, res) => {
   const { from, text } = req.body?.entry[0]?.changes[0]?.value?.messages[0];
   const { body } = text;
-  axios
-    .post('https://tan-powerful-hummingbird.cyclic.app/dialogflow', {
-      languageCode: 'en',
-      queryText: body,
-      sessionId: from,
-    })
-    .then((response) => {
-      console.log(response.data);
-    });
+  const responseMessage = await callDialogFlow(body, from);
+  const send = await sendMessage(from, responseMessage);
+  res
+    .status(200)
+    .json({ success: true, data: send, message: 'Message sent successfully' });
 });
 
 export default router;
